175a4b7c93615fc3b6d5a01f2a793047f37ebe4a
taylorallen <twhitallen07@gmail.com>
2025-07-22 19:53:25 -0500

    updated placeholder

--- frontend/src/pages/StudentList.jsx
+++ frontend/src/pages/StudentList.jsx
@@ -332,7 +332,7 @@ export function StudentList({ onRefresh }) {
       <div className="relative">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
         <Input
-          placeholder="Search students or work descriptions..."
+          placeholder="Search students..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           className="pl-10"
